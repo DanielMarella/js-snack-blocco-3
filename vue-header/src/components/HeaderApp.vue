537f7bb8cad3a0198e197f08d117eddf15b75ce5
DanielMarella <marella.daniel96@gmail.com>
2023-06-15 12:48:25 +0200

    adding some tag to create a list in flex, add some style

--- vue-header/src/components/HeaderApp.vue
+++ vue-header/src/components/HeaderApp.vue
@@ -1,5 +1,16 @@
 <template>
-<p>ciaooo</p>
+  <header>
+    <div>
+      <ul>
+        <li>Questo</li>
+        <li>modo</li>
+        <li>di</li>
+        <li>fare</li>
+        <li>mi</li>
+        <li>gasa</li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +20,16 @@ export default {
 </script>
 
 <style lang="scss">
-  
+
+  ul{
+    display: flex;
+  }
+
+  li{
+    font-size: 2rem;
+    margin: 0 10px;
+    text-align: center;
+    width: 100px;
+    border: 5px solid violet;
+  }
 </style>
